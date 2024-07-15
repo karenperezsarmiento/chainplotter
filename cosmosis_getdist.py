743e0a79c865c635b9174cbbbd608db6f52c9589
karenperezsarmiento <kaper@sas.upenn.edu>
2024-07-15 18:03:36 -0500

    added mock code

--- cosmosis_getdist.py
+++ cosmosis_getdist.py
@@ -1,4 +1,12 @@
 import numpy as np
 import getdist
+from getdist import MCSamples
 from getdist import loadMCSamples
 
+def loadCosmosisMCSamples(filename):
+    #load filename
+    #do some stuff to the filename like the getdist.loadMCSamples function does
+    #package everything into a getdist MCSamples object
+    samples = MCSamples
+    #return
+    return samples
