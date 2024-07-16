3c3b2859ef902120586a9a920c873f632aa7d92a
karenperezsarmiento <kaper@sas.upenn.edu>
2024-07-16 13:26:04 -0500

    added functionality for weights and loglikes

--- cosmosis_getdist.py
+++ cosmosis_getdist.py
@@ -2,6 +2,7 @@ import numpy as np
 import getdist
 from getdist import MCSamples
 from getdist import loadMCSamples
+import re
 
 class loadCosmosisMCSamples:
     #load filename
@@ -13,7 +14,14 @@ class loadCosmosisMCSamples:
         self.chainfile = filename+".txt"
         self.get_metadata()
         self.get_columnnames()
+        self.get_sampler_type()
         self.get_chains()
+        self.index_log,self.index_weight,self.index_samples = self.get_indices()
+        self.get_paramnames()
+        self.get_samples()
+        self.get_loglikes()
+        self.get_weights()
+
 
     def get_metadata(self):
         metadata = []
@@ -22,46 +30,60 @@ class loadCosmosisMCSamples:
                 if line.startswith("#"):
                     clean_line = line.strip("#")
                     metadata.append(clean_line)
-        self.metadata = metadata
-        return self.metadata    
+        self.metadata = metadata   
 
     def get_columnnames(self):
         if self.metadata == None:
             self.metadata = self.get_metadata(self)
         colnames = self.metadata[0].split("\t")
         self.colnames = colnames
-        return self.colnames
+    
+    def get_indices(self):
+        index_log = np.where(np.array(self.colnames) == "post")[0]
+        index_all = np.arange(len(self.colnames))
+        index_weight = np.where(np.array(self.colnames) == "weight\n")[0]
+        index_samples = list(np.array(np.delete(index_all,[index_log,index_weight]).astype(int)))
+        return index_log,index_weight,index_samples
     
     def get_samples(self):
-        samples = []
-        self.samples = samples
-        return self.samples
+        self.samples = self.chains[:, self.index_samples]
 
     def get_chains(self):
         chains = np.loadtxt(self.chainfile,comments="#")
         self.chains = chains
-        return self.chains
-    
-    def get_weights(self):
-        for i,cn in enumerate(self.colnames):
-            if "weight" in cn:
-                weight_ind = i
 
-        return self.weights
+    def get_sampler_type(self):
+        for i in self.metadata:
+            if "sampler" in i:
+                sampler = re.sub("sampler = ","",i)
+                if sampler == "polychord":
+                    self.sampler_type = "nested"
+    
+
+
+    def get_weights(self):
+        self.weights = self.chains[:,self.index_weight]
     
     def get_loglikes(self):
-        return 0
+        self.log = self.chains[:,self.index_log]
         
     def get_paramnames(self):
-        return 0
+        print(type(self.index_samples[0]))
+        self.paramnames = self.colnames[self.index_samples]
     
     def get_labels(self):
         return 0
 
-    def get_sampler(self):
-        return 0
-
     def get_ranges(self):
+        return 0
+    
+    def make_sampler(self):
+        self.mc_samples = MCSamples(samples=self.samples, weights=self.weights,
+                           loglikes=-2.*loglike,
+                           sampler=sampler, names=param_names,
+                           labels=param_labels, ranges=ranges,
+                           ignore_rows=0, name_tag=name_tag,
+                           settings=settings)
         return 0
 
 
